a6c81fc47039c900853358bac68ff0b61fd5690e
Andrew Champion <andrew.champion@gmail.com>
2017-03-01 14:19:46 -0500

    CLI: reformat indendation (minor)

--- diluvian/__main__.py
+++ diluvian/__main__.py
@@ -22,70 +22,88 @@ def _make_main_parser():
     """
     common_parser = argparse.ArgumentParser(add_help=False)
 
-    common_parser.add_argument('-c', '--config-file', action='append', dest='config_files', default=[],
-                               help='Configuration files to use. For defaults, see `conf/default.toml`. '
-                                    'Values are overwritten in the order provided.')
-    common_parser.add_argument('-m', '--model-file', dest='model_file', default=None,
-                               help='Existing network model file to use for prediction or continued training.')
-    common_parser.add_argument('-v', '--volume-file', dest='volume_file', default=None,
-                               help='Volume configuration file. For example, see `conf/cremi_datasets.toml`.')
-    common_parser.add_argument('--no-in-memory', action='store_false', dest='in_memory', default=True,
-                               help='Do not preload entire volumes into memory.')
-    common_parser.add_argument('-l', '--log', dest='log_level',
-                               choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                               help='Set the logging level.')
+    common_parser.add_argument(
+            '-c', '--config-file', action='append', dest='config_files', default=[],
+            help='Configuration files to use. For defaults, see `conf/default.toml`. '
+                 'Values are overwritten in the order provided.')
+    common_parser.add_argument(
+            '-m', '--model-file', dest='model_file', default=None,
+            help='Existing network model file to use for prediction or continued training.')
+    common_parser.add_argument(
+            '-v', '--volume-file', dest='volume_file', default=None,
+            help='Volume configuration file. For example, see `conf/cremi_datasets.toml`.')
+    common_parser.add_argument(
+            '--no-in-memory', action='store_false', dest='in_memory', default=True,
+            help='Do not preload entire volumes into memory.')
+    common_parser.add_argument(
+            '-l', '--log', dest='log_level',
+            choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+            help='Set the logging level.')
 
     parser = argparse.ArgumentParser(description='Train or run flood-filling networks on EM data.')
 
     commandparsers = parser.add_subparsers(help='Commands', dest='command')
 
-    train_parser = commandparsers.add_parser('train', parents=[common_parser],
-                                             help='Train a network from labeled volumes.')
-    train_parser.add_argument('-mo', '--model-output-filebase', dest='model_output_filebase', default=None,
-                              help='Base filename for the best trained model and other output artifacts, '
-                                   'such as metric plots and configuration state.')
-    train_parser.add_argument('-mc', '--model-checkpoint-file', dest='model_checkpoint_file', default=None,
-                              help='Filename for model checkpoints at every epoch. '
-                                   'This is different that the model output file; if provided, this HDF5 model '
-                                   'file is saved every epoch regardless of validation performance.'
-                                   'Can use Keras format arguments: https://keras.io/callbacks/#modelcheckpoint')
-    train_parser.add_argument('--tensorboard', action='store_true', dest='tensorboard', default=False,
-                              help='Output tensorboard log files while training.')
-    train_parser.add_argument('--viewer', action='store_true', dest='viewer', default=False,
-                              help='Create a neuroglancer viewer for a training sample at the end of training.')
-    train_parser.add_argument('--metric-plot', action='store_true', dest='metric_plot', default=False,
-                              help='Plot metric history at the end of training. '
-                                   'Will be saved as a PNG with the model output base filename.')
+    train_parser = commandparsers.add_parser(
+            'train', parents=[common_parser],
+            help='Train a network from labeled volumes.')
+    train_parser.add_argument(
+            '-mo', '--model-output-filebase', dest='model_output_filebase', default=None,
+            help='Base filename for the best trained model and other output artifacts, '
+                 'such as metric plots and configuration state.')
+    train_parser.add_argument(
+            '-mc', '--model-checkpoint-file', dest='model_checkpoint_file', default=None,
+            help='Filename for model checkpoints at every epoch. '
+                 'This is different that the model output file; if provided, this HDF5 model '
+                 'file is saved every epoch regardless of validation performance.'
+                 'Can use Keras format arguments: https://keras.io/callbacks/#modelcheckpoint')
+    train_parser.add_argument(
+            '--tensorboard', action='store_true', dest='tensorboard', default=False,
+            help='Output tensorboard log files while training.')
+    train_parser.add_argument(
+            '--viewer', action='store_true', dest='viewer', default=False,
+            help='Create a neuroglancer viewer for a training sample at the end of training.')
+    train_parser.add_argument(
+            '--metric-plot', action='store_true', dest='metric_plot', default=False,
+            help='Plot metric history at the end of training. '
+                 'Will be saved as a PNG with the model output base filename.')
 
-    fill_parser = commandparsers.add_parser('fill', parents=[common_parser],
-                                            help='Use a trained network to fill random regions in a volume.')
+    fill_parser = commandparsers.add_parser(
+            'fill', parents=[common_parser],
+            help='Use a trained network to fill random regions in a volume.')
     fill_parser.add_argument(
             '-bi', '--bounds-input-file', dest='bounds_input_file', default=None,
-            help='Filename for bounds CSV input. Should contain "{volume}", which will be substituted with '
-                 'the volume name for each respective volume\'s bounds.')
-    fill_parser.add_argument('--no-bias', action='store_false', dest='bias', default=True,
-                             help='Overwrite prediction mask at the end of each field of view inference '
-                                  'rather than using the anti-merge bias update.')
-    fill_parser.add_argument('--move-batch-size', dest='move_batch_size', default=1, type=int,
-                             help='Maximum number of fill moves to process in each prediction batch.')
-    fill_parser.add_argument('--max-moves', dest='max_moves', default=None, type=int,
-                             help='Cancel filling after this many moves.')
-    fill_parser.add_argument('--multi-gpu-model-kludge', dest='multi_gpu_model_kludge', default=None, type=int,
-                             help='Fix using a multi-GPU trained model that was not saved properly by '
-                                  'setting this to the number of training GPUs.')
+            help='Filename for bounds CSV input. Should contain "{volume}", which will be '
+                 'substituted with the volume name for each respective volume\'s bounds.')
+    fill_parser.add_argument(
+            '--no-bias', action='store_false', dest='bias', default=True,
+            help='Overwrite prediction mask at the end of each field of view inference '
+                 'rather than using the anti-merge bias update.')
+    fill_parser.add_argument(
+            '--move-batch-size', dest='move_batch_size', default=1, type=int,
+            help='Maximum number of fill moves to process in each prediction batch.')
+    fill_parser.add_argument(
+            '--max-moves', dest='max_moves', default=None, type=int,
+            help='Cancel filling after this many moves.')
+    fill_parser.add_argument(
+            '--multi-gpu-model-kludge', dest='multi_gpu_model_kludge', default=None, type=int,
+            help='Fix using a multi-GPU trained model that was not saved properly by '
+                 'setting this to the number of training GPUs.')
 
-    check_config_parser = commandparsers.add_parser('check-config', parents=[common_parser],
-                                                    help='Check a configuration value.')
-    check_config_parser.add_argument('config_property', default=None, nargs='?',
-                                     help='Name of the property to show, e.g., `training.batch_size`.')
+    check_config_parser = commandparsers.add_parser(
+            'check-config', parents=[common_parser],
+            help='Check a configuration value.')
+    check_config_parser.add_argument(
+            'config_property', default=None, nargs='?',
+            help='Name of the property to show, e.g., `training.batch_size`.')
 
     gen_subv_bounds_parser = commandparsers.add_parser(
             'gen-subv-bounds', parents=[common_parser],
             help='Generate subvolume bounds.')
     gen_subv_bounds_parser.add_argument(
             'bounds_output_file', default=None,
-            help='Filename for the CSV output. Should contain "{volume}", which will be substituted with '
-                 'the volume name for each respective volume\'s bounds.')
+            help='Filename for the CSV output. Should contain "{volume}", which will be '
+                 'substituted with the volume name for each respective volume\'s bounds.')
     gen_subv_bounds_parser.add_argument(
             'num_bounds', default=None, type=int,
             help='Number of bounds to generate.')
