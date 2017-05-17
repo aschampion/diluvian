Regions: fix off-by-one error in move bounds
This only affects the edge case where a MOVE_DELTA is 1.

diluvian/regions.py
@@ -87,7 +87,7 @@ class Region(object):
         self.move_bounds = (
             np.ceil(np.divide((CONFIG.model.input_fov_shape - 1) / 2 - self.MOVE_GRID_OFFSET,
                               self.MOVE_DELTA)).astype(np.int64),
-            self.vox_to_pos(self.bounds - (CONFIG.model.input_fov_shape - 1) / 2),
+            self.vox_to_pos(np.array(self.bounds) - 1 - (CONFIG.model.input_fov_shape - 1) / 2),
             )
         self.move_check_thickness = CONFIG.model.move_check_thickness
         if mask is None: