Regions: fix off-by-one error in move checking
There are only two hard problems in CS...

diluvian/regions.py
@@ -232,7 +232,7 @@ class Region(object):
             in the move plane in that direction.
         """
         moves = []
-        ctr = (np.asarray(mask.shape) - 1) // 2 + 1
+        ctr = np.asarray(mask.shape) // 2
         for move in map(np.array, [(1, 0, 0), (-1, 0, 0),
                                    (0, 1, 0), (0, -1, 0),
                                    (0, 0, 1), (0, 0, -1)]):

tests/test_diluvian.py
@@ -9,6 +9,8 @@ Tests for `diluvian` module.
 """
 
 
+from __future__ import division
+
 import numpy as np
 
 from diluvian import octrees
@@ -76,7 +78,7 @@ def test_region_moves():
     mock_image = np.zeros(tuple(CONFIG.model.training_subv_shape), dtype=np.float32)
     region = regions.Region(mock_image)
     mock_mask = np.zeros(tuple(CONFIG.model.output_fov_shape), dtype=np.float32)
-    ctr = np.array(mock_mask.shape) / 2 + 1
+    ctr = np.array(mock_mask.shape) // 2
     expected_moves = {}
     for i, move in enumerate(map(np.array, [(1, 0, 0), (-1, 0, 0),
                                             (0, 1, 0), (0, -1, 0),